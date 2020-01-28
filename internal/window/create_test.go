feec58ce398d418f5d4d7cb3f34c3dccded6e1ef
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-01-28 23:46:06 +0100

    Readded ScaleToMonitor hint

--- internal/window/create_test.go
+++ internal/window/create_test.go
@@ -3,11 +3,15 @@ package window
 import (
 	"testing"
 
+	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(t *testing.M) {
-	Init()
+	err := Init()
+	if err != nil {
+		panic(err)
+	}
 	t.Run()
 	Terminate()
 }
@@ -19,6 +23,8 @@ func TestCreateWindowNormal(t *testing.T) {
 	win, err := NewWindow(hints, "Test Window", 800, 450, nil)
 	assert.NoError(t, err)
 	assert.NotNil(t, win)
+
+	win.Destroy()
 }
 
 func TestCreateWindowMaximized(t *testing.T) {
@@ -29,4 +35,24 @@ func TestCreateWindowMaximized(t *testing.T) {
 	win, err := NewWindow(hints, "Test Window", 800, 450, nil)
 	assert.NoError(t, err)
 	assert.NotNil(t, win)
+
+	win.Destroy()
+}
+
+func TestCreateWindowScaledToMon(t *testing.T) {
+	monitor := glfw.GetPrimaryMonitor()
+	vidMode := monitor.GetVideoMode()
+
+	hints := NewHints()
+	hints.ContextVersionMajor.Value = 3
+	hints.ContextVersionMinor.Value = 2
+	hints.Maximized.Value = true
+	hints.ScaleToMonitor.Value = true
+	win, err := NewWindow(hints, "Test Window", 800, 450, nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, win)
+	w, h := win.GetSize()
+	assert.Equal(t, vidMode.Width, w, h)
+
+	win.Destroy()
 }
