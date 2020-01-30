c11b83f97a0e91f62d11f87281c3a9770c8af984
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-01-30 11:34:16 +0100

    Postponing headful testing

--- build/package/entryfile.sh
+++ build/package/entryfile.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 cd /root/src/
-xvfb-run -e /dev/stderr --server-args=':99 -screen 0 640x480x8 +extension GLX +render -noreset -ac' go test ./...
+##xvfb-run -e /dev/stderr --server-args=':99 -screen 0 640x480x8 +extension GLX +render -noreset -ac' glxinfo
+#Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile /dev/stdout -config /xorg.conf :10 &
+#sleep 2
+#echo ================
+#export DISPLAY=:10
+#vglrun -d :10 glxinfo
+#echo ================
+#vglrun go test --tags=headless ./...
+go test --tags=headless ./...
