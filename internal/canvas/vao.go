4bf8cbab51c6217301c564278206c1dd9e07757c
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-01-30 23:25:38 +0100

    Added Vao

---
+++ internal/canvas/vao.go
@@ -0,0 +1,37 @@
+package canvas
+
+import (
+	"github.com/Qendolin/go-printpixel/internal"
+	"github.com/go-gl/gl/v3.2-core/gl"
+)
+
+type Vao struct {
+	*uint32
+}
+
+func NewVao() *Vao {
+	id := new(uint32)
+	gl.GenVertexArrays(1, id)
+	return &Vao{id}
+}
+
+func (vao *Vao) Id() uint32 {
+	return *vao.uint32
+}
+
+func (vao *Vao) Bind() {
+	gl.BindVertexArray(vao.Id())
+}
+
+func (vao *Vao) Unbind() {
+	gl.BindVertexArray(0)
+}
+
+func (vao *Vao) BindFor(context internal.BindingClosure) {
+	vao.Bind()
+	defered := context()
+	vao.Unbind()
+	for _, deferedFunc := range defered {
+		deferedFunc()
+	}
+}
