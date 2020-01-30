b2ca769609fc5ea263b7e72cf9dd6d1287b09ccc
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-01-30 20:21:22 +0100

    Added Vbo

---
+++ internal/canvas/vbo.go
@@ -0,0 +1,41 @@
+package canvas
+
+import "github.com/go-gl/gl/v3.2-core/gl"
+
+import "encoding/binary"
+
+type Vbo struct {
+	*uint32
+}
+
+func NewVbo() *Vbo {
+	vbo := new(uint32)
+	gl.GenBuffers(1, vbo)
+	return &Vbo{vbo}
+}
+
+func (vbo *Vbo) Id() uint32 {
+	return *vbo.uint32
+}
+
+func (vbo *Vbo) Bind(target uint32) {
+	gl.BindBuffer(target, vbo.Id())
+}
+
+/*
+	data - a silce of some type
+*/
+func (vbo *Vbo) WriteStatic(data interface{}, length int, dataType interface{}) {
+	vbo.Bind(gl.ARRAY_BUFFER)
+	size := binary.Size(data)
+	if size == -1 {
+		//TODO
+		panic("invalid data type")
+	}
+	gl.BufferData(gl.ARRAY_BUFFER, size, gl.Ptr(data), gl.STATIC_DRAW)
+}
+
+func (vbo *Vbo) Destroy() {
+	gl.DeleteBuffers(1, vbo.uint32)
+	vbo.uint32 = nil
+}
