83c0b0cc0feb34540db5b6489f07c17afc6e6514
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-02-05 01:54:25 +0100

    Removed testing code

--- internal/profiler/proxy_windows.go
+++ internal/profiler/proxy_windows.go
@@ -1,4 +1,3 @@
 package profiler
 
-//--go:generate cmd /C ..\..\scripts\generate-proxies.cmd
-//go:generate bash ../../scripts/generate-proxies.cmd
+//go:generate cmd /C ..\..\scripts\generate-proxies.cmd
