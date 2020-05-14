77575ac6f74b313da8f72ba10234bd92591d1337
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-05-14 15:24:28 +0200

    Cleaned up docker test

--- build/package/entryfile.sh
+++ build/package/entryfile.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 cd /root/src/
-echo Starting Xvfb
-export DISPLAY=:10
-Xvfb :10 -screen 0 1024x768x24 +extension GLX +render -noreset -ac &
-echo Installing go packages
 go get -v -t -d ./...
 echo Starting Test
-go test -timeout 120s ./... -headless
+#xvfb-run -e /dev/stderr glxinfo
+xvfb-run -e /dev/stderr go test -timeout 120s ./... -headless
