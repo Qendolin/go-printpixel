faa144ff519a069c3fe969708d4a16e34aadbe8a
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-06-22 23:18:04 +0200

    Implemented viewer

--- layout/viewer.go
+++ layout/viewer.go
@@ -1,14 +1,48 @@
 package layout
 
-import "github.com/Qendolin/go-printpixel/internal/canvas"
+import (
+	"github.com/Qendolin/go-printpixel/internal/data"
+	"github.com/Qendolin/go-printpixel/internal/renderer"
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+var _viewCopyer *renderer.TextureCopy
 
 type Viewer struct {
-	Canvas canvas.Canvas
+	Target renderer.TextureQuad
 	SimpleBox
 }
 
+func NewViewer() *Viewer {
+	return &Viewer{
+		Target: *renderer.NewTextureQuad(),
+	}
+}
+
+func viewCopyer() *renderer.TextureCopy {
+	if _viewCopyer == nil {
+		_viewCopyer = renderer.NewTextureCopy()
+	}
+	return _viewCopyer
+}
+
+func (v *Viewer) Layout() {
+	vc := viewCopyer()
+
+	vc.SetSource(*v.Target.Texture)
+
+	dst := data.NewTexture2D(data.Tex2DTarget2D)
+	dst.Bind(0)
+	dst.AllocEmpty(0, gl.RGB, int32(v.width), int32(v.height), gl.RGB)
+	dst.ApplyDefaults()
+	vc.SetDestination(*dst)
+	vc.Bind()
+	vc.Draw()
+	vc.Unbind()
+}
+
 func (v Viewer) Draw() {
-	v.Canvas.Bind()
-	v.Canvas.Draw()
-	v.Canvas.Unbind()
+	v.Target.Bind()
+	v.Target.Draw()
+	v.Target.Unbind()
 }
