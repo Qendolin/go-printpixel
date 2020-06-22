f73a5d0bc8b1bc69fdb4e6d5a7b385e95b703f0c
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-06-23 01:56:07 +0200

    Added examples

---
+++ examples/gol/gol.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Qendolin/go-printpixel/layout"
+	"github.com/Qendolin/go-printpixel/window"
+	"github.com/go-gl/gl/v2.1/gl"
+)
+
+const (
+	Width  = 1024
+	Height = 1024
+)
+
+func main() {
+	win := setup()
+
+	view := layout.NewViewer()
+	view.Target.Texture.Bind(0)
+	view.Target.Texture.ApplyDefaults()
+	view.Target.Texture.AllocEmpty(0, gl.RGB, Width, Height, gl.RGB)
+	win.Child = view
+
+	life := NewLife(Width, Height)
+	win.BeforeUpdate = func() {
+		life.Step()
+		tex := life.Texture()
+		view.Target.Texture.Bind(0)
+		view.Target.Texture.WriteBytes(tex, 0, 0, 0, Width, Height, gl.RGB)
+		view.Draw()
+		fmt.Println(win.Window.Delta())
+	}
+
+	win.Run()
+	win.Close()
+}
+
+func setup() window.Layout {
+	hints := window.NewHints()
+	hints.Resizable.Value = false
+	hints.Vsync.Value = false
+	win, err := window.New(hints, "Game of Life Example", Width, Height, nil)
+	panicIf(err)
+
+	glCfg := window.NewGlConfig(0)
+	glCfg.Debug = true
+	go handleErrors(glCfg.Errors)
+	err = win.Init(glCfg)
+	panicIf(err)
+
+	return win
+}
+
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handleErrors(errs <-chan window.GlError) {
+	for err := range errs {
+		if err.Fatal {
+			log.Fatalf("%v\n%v", err, err.Stack)
+		}
+		log.Printf("%v\n", err)
+	}
+}
