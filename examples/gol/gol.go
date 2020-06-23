92425a687f5794f808a1341860ad80e7783f91ed
Qendolin <32160662+Qendolin@users.noreply.github.com>
2020-06-23 02:57:06 +0200

    Fixed gl version

--- examples/gol/gol.go
+++ examples/gol/gol.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/Qendolin/go-printpixel/layout"
 	"github.com/Qendolin/go-printpixel/window"
-	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/gl/v3.3-core/gl"
 )
 
 const (
